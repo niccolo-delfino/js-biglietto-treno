e1b13dc21f9e41512fad61c27454e4fd50c70ebf
Niccolo Delfino <delfino.nicco@libero.it>
2020-12-14 16:11:01 +0100

    all calc ok, now writing on html

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 var km = parseInt(prompt('Quanti Kilometri devi percorrere?'));
 console.log(km);
+document.getElementById('km').innerHTML += km
 
 var eta = parseInt(prompt('Quanti anni hai?'));
 console.log(eta);
+document.getElementById('eta').innerHTML += eta
 
 var prezzo = km * 0.21
 console.log(prezzo);
+document.getElementById('prezzo').innerHTML += prezzo
 
 var sconto = 'Nullo'
 console.log(sconto);
@@ -15,10 +18,12 @@ if (eta <= 17) {
 } else if (eta >= 65) {
   console.log(sconto = (prezzo * 40) / 100);
 }
+document.getElementById('sconto').innerHTML += sconto
 
 var prezzoFinale = prezzo;
 console.log(prezzoFinale);
 
 if (sconto != 'Nullo') {
-  console.log(prezzoFinale = prezzo - sconto);
+  console.log(prezzoFinale += prezzo - sconto);
 }
+document.getElementById('prezzoFinale').innerHTML += prezzoFinale
